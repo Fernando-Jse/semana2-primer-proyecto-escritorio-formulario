4268c23b6a01373cc58aae961052d9b7fd8aa654
Fernando José López Torres <USTS002422@ugb.edu.sv>
2022-07-25 21:00:08 -0600

    Ya programe el button en donde generara los nombres que conforma mi equipo

--- Form1.cs
+++ Form1.cs
@@ -20,5 +20,10 @@ namespace semana2_primer_proyecto_escritorio_formulario
         private void button1_Click(object sender, EventArgs e)
         {
             label1.Text = "Fernando José";
-    }
+            label2.Text = "Edwin Emerson";
+            label3.Text = "Nilderson Alexis";
+            label4.Text = "Diego Enmanuel";
+            label5.Text = "Henry Anderson";
+            label6.Text = "Christian Alexander";
+        }
 }
